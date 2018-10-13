7c59ce913b23e6d00bfa30161aad30ddedc18862
pgr0101 <pgr0101mm@mehr.ui.ac.ir>
2018-10-13 19:48:38 +0330

    try if it works

--- src/chessgame/ChessGame.java
+++ src/chessgame/ChessGame.java
@@ -16,6 +16,7 @@ public class ChessGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("try if it works");
     }
     
 }
